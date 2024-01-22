2fd3f347198eb1e767080b4c62e0be3ab7ca13d5
capYousef <elcaptain.yousef.official@gmail.com>
2024-01-22 11:39:57 +0200

    -

--- app/Http/Middleware/AssignGuard.php
+++ app/Http/Middleware/AssignGuard.php
@@ -24,21 +24,23 @@ class AssignGuard extends BaseMiddleware
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if($guard != null){
+        $token = $request->header('auth-token');
+        if (!$token) {
+            return $this->returnError('401', 'Token not provided');
+        }
+        if ($guard != null) {
             auth()->shouldUse($guard); //shoud you user guard / table
-            $token = $request->header('auth-token');
             $request->headers->set('auth-token', (string) $token, true);
-            $request->headers->set('Authorization', 'Bearer '.$token, true);
+            $request->headers->set('Authorization', 'Bearer ' . $token, true);
             try {
-            //    $user = $this->auth->authenticate($request);  //check authenticted user
+                //    $user = $this->auth->authenticate($request);  //check authenticted user
                 $user = JWTAuth::parseToken()->authenticate();
             } catch (TokenExpiredException $e) {
-                return  $this -> returnError('401','Unauthenticated user');
+                return  $this->returnError('401', 'Unauthenticated user');
             } catch (JWTException $e) {
-                return  $this -> returnError('', $e->getMessage());
+                return  $this->returnError('', $e->getMessage());
             }
-
         }
-         return $next($request);
+        return $next($request);
     }/* end of function */
 }
