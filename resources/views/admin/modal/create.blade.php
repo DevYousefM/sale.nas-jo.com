16bcfe5e59dce12df3aa327a387931585dcfff2a
capYousef <elcaptain.yousef.official@gmail.com>
2024-01-23 01:11:26 +0200

    create modal view

--- resources/views/admin/modal/create.blade.php
+++ resources/views/admin/modal/create.blade.php
@@ -3,48 +3,80 @@
 @section('content')
     <!-- Content wrapper -->
     <div class="content-wrapper">
-    <!-- Content -->
+        <!-- Content -->
         <div class="container-xxl flex-grow-1 container-p-y">
-            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"><a href="{{ route('category.index') }}"> {{ __('admin.subcategories') }} </a> / </span> {{ __('admin.add') }}</h4>
+            <h4 class="fw-bold py-3 mb-4">
+                <span class="text-muted fw-light">
+                    <a href="{{ route('category.index') }}"> {{ __('admin.subcategories') }} </a> /
+                </span>
+                {{ __('admin.add') }}
+            </h4>
             <div class="row mb-4">
                 <div class="row">
                     <!-- FormValidation -->
                     <div class="col-12">
                         <div class="card">
-                        <h5 class="card-header"> {{ __('admin.add_new_modal') }}</h5>
-                        <div class="card-body">
-                            @include('admin.includes._errors')
-                            <form id="formValidationExamples" class="row g-3" action="{{ route('modals.store') }}" method="POST">
-                                @csrf
+                            <h5 class="card-header"> {{ __('admin.add_new_modal') }}</h5>
+                            <div class="card-body">
+                                @include('admin.includes._errors')
+                                <form id="formValidationExamples" class="row g-3" action="{{ route('modals.store') }}" method="POST">
+                                    @csrf
 
-                               
-                                @foreach (config('translatable.locales') as $lang)
-
-                                    <div class="mb-3 row">
-                                        <label for="html5-text-input" class="col-md-2 col-form-label">{{ __('admin.brand_'.$lang) }}</label>
-                                        <div class="col-md-10 col-lg-7">
-                                          <input class="form-control" type="text" name="brand:{{ $lang }}" id="html5-text-input" />
+                                    @foreach (config('translatable.locales') as $lang)
+                                        <div class="mb-3 row">
+                                            <label for="brand_{{ $lang }}" class="col-md-2 col-form-label">{{ __('admin.brand_'.$lang) }}</label>
+                                            <div class="col-md-10 col-lg-7">
+                                                <input class="form-control" type="text" name="brand:{{ $lang }}" id="brand_{{ $lang }}" />
+                                            </div>
                                         </div>
-                                    </div>
-                                @endforeach
-                                @foreach (config('translatable.locales') as $lang)
-                                    <div class="mb-3 row">
-                                        <label for="html5-text-input" class="col-md-2 col-form-label">{{ __('admin.modal_'.$lang) }}</label>
-                                        <div class="col-md-10 col-lg-7">
-                                          <input class="form-control" type="text" name="modal:{{ $lang }}" id="html5-text-input" />
-                                        </div>
-                                    </div>
-                                @endforeach                                
+                                    @endforeach
 
-                                <div class="col-12">
-                                    <button type="submit"  class="btn btn-primary">{{ __('admin.add') }}</button>
-                                </div>
-                            </form>
-                        </div>
+                                    @foreach (config('translatable.locales') as $lang)
+                                        <div class="mb-3 row">
+                                            <label for="modal_{{ $lang }}" class="col-md-2 col-form-label">{{ __('admin.modal_'.$lang) }}</label>
+                                            <div class="col-md-10 col-lg-7">
+                                                <input class="form-control" type="text" name="modal:{{ $lang }}[]" id="modal_{{ $lang }}" />
+                                            </div>
+                                        </div>
+                                    @endforeach
+
+                                    <div class="col-12">
+                                        <button type="button" class="btn btn-primary" id="addModalField">{{ __('admin.add_modal_field') }}</button>
+                                    </div>
+
+                                    <div id="additionalModalFields"></div>
+
+                                    <div class="col-12 mt-3">
+                                        <button type="submit" class="btn btn-primary">{{ __('admin.add') }}</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var modalCounter = 1;
+
+            document.getElementById('addModalField').addEventListener('click', function () {
+                modalCounter++;
+
+                @foreach (config('translatable.locales') as $lang)
+                    var additionalModalField = `
+                        <div class="mb-3 row">
+                            <label for="additional_modal_{{ $lang }}_${modalCounter}" class="col-md-2 col-form-label">{{ __('admin.modal_'.$lang) }} ${modalCounter}</label>
+                            <div class="col-md-10 col-lg-7">
+                                <input class="form-control" type="text" name="modal:{{ $lang }}[]" id="additional_modal_{{ $lang }}_${modalCounter}" />
+                            </div>
+                        </div>
+                    `;
+                    document.getElementById('additionalModalFields').insertAdjacentHTML('beforeend', additionalModalField);
+                @endforeach
+            });
+        });
+    </script>
 @endsection
