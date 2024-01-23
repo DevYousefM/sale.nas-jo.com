bd6032c472a28286ced06f8ce013fb20b15ef4b7
capYousef <elcaptain.yousef.official@gmail.com>
2024-01-23 16:01:43 +0200

    -

--- app/Http/Controllers/Api/Client/CategoryController.php
+++ app/Http/Controllers/Api/Client/CategoryController.php
@@ -14,53 +14,60 @@ class CategoryController extends Controller
 {
     use GeneralTrait;
 
-    public function all(Request $request){
+    public function all(Request $request)
+    {
         $lang = $request->header('Accept-Language');
-        if($lang){
+        if ($lang) {
             App::setLocale($lang);
         }
         $categories = Category::all();
-        return $this->returnData('data' , $categories);
-    }//end of all function
+        return $this->returnData('data', $categories);
+    } //end of all function
 
-    public function getSubCategoriesByID($id,Request $request){
+    public function getSubCategoriesByID($id, Request $request)
+    {
         $lang = $request->header('Accept-Language');
-        if($lang){
+        if ($lang) {
             App::setLocale($lang);
         }
         $category = Category::find($id);
-        if($category){
-            return $this->returnData('data' , $category->subcategories);
-        }else{
-            return $this->returnError('001',__('api.item_not_found'));
+        if ($category) {
+            return $this->returnData('data', $category->subcategories);
+        } else {
+            return $this->returnError('001', __('api.item_not_found'));
         }
-    }//end of getSubCategoriesByID function
+    } //end of getSubCategoriesByID function
 
-    public function all_subcategories(Request $request){
+    public function all_subcategories(Request $request)
+    {
         $lang = $request->header('Accept-Language');
-        if($lang){
+        if ($lang) {
             App::setLocale($lang);
         }
-    
+
         // Get all subcategories
         $subcategories = SubCategory::all();
-    
-        // Extract subcategory names
-        $subcategoryNames = $subcategories->pluck('name')->toArray();
-    
+
+        // Extract subcategory names in the current language
+        $subcategoryNames = [];
+        foreach ($subcategories as $subcategory) {
+            $subcategoryNames[] = $subcategory->translateOrDefault($lang)->name;
+        }
+
         // Search for Modals with brand equal to subcategory names
         $modals = Modal::whereIn('brand', $subcategoryNames)->get();
-    
-        return $this->returnData('data' , $modals);
-    }//end of all_subcategories function
 
-    public function getFeaturesBySubCategoryID($id,Request $request){
+        return $this->returnData('data', $modals);
+    } //end of all_subcategories function
+
+    public function getFeaturesBySubCategoryID($id, Request $request)
+    {
         $lang = $request->header('Accept-Language');
-        if($lang){
+        if ($lang) {
             App::setLocale($lang);
         }
         $subcategory = SubCategory::findOrFail($id);
-        return $this->returnData('data' , $subcategory->features);
-    }//end of function
+        return $this->returnData('data', $subcategory->features);
+    } //end of function
 
 }//end of class
